1. fix MOSNoiseTB calculation so it will still work with 1 corner    simulation.

verification_ec/mos/sim.py
@@ -369,10 +369,12 @@ class MOSNoiseTB(TestbenchManager):
         idn = np.log(scale / fg * (idn ** 2))
         delta_list = [1e-6] * (len(ss_swp_names) + 1)
         delta_list[-1] = 1e-3
-        noise_fun = LinearInterpolator(cur_points, idn, delta_list, extrapolate=True)
-        integ_noise = noise_fun.integrate(fstart_log, fstop_log, axis=-1, logx=True, logy=True, raw=True)
+        integ_noise_list = []
+        for idx in range(len(corner_list)):
+            noise_fun = LinearInterpolator(cur_points, idn[idx, ...], delta_list, extrapolate=True)
+            integ_noise_list.append(noise_fun.integrate(fstart_log, fstop_log, axis=-1, logx=True, logy=True, raw=True))
 
-        gamma = integ_noise / (4.0 * 1.38e-23 * temp * ss_data['gm'] * (fstop - fstart))
+        gamma = np.array(integ_noise_list) / (4.0 * 1.38e-23 * temp * ss_data['gm'] * (fstop - fstart))
         self.record_array(ss_data, data, gamma, 'idn_integ', new_swp_vars)
         return ss_data
 