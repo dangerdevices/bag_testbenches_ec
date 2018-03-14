f1a9ce942572be9f417e6693e94591d02e034e39
Eric Chang <pkerichang@gmail.com>
2018-03-14 09:47:00 -0700

    finish opamp analysis script.

--- scripts_char/opamp_plot.py
+++ scripts_char/opamp_plot.py
@@ -6,7 +6,8 @@ import scipy.signal as scisig
 
 import matplotlib.pyplot as plt
 
-from bag.data.lti import LTICircuit
+from bag.util.search import FloatBinaryIterator
+from bag.data.lti import LTICircuit, get_stability_margins, get_w_crossings
 
 from verification_ec.mos.query import MOSDBDiscrete
 
@@ -22,40 +23,6 @@ def get_db(nch_dir, pch_dir, intent='standard', interp_method='spline', sim_env=
     pch_db.set_dsn_params(intent=intent)
 
     return nch_db, pch_db
-
-
-def tf_vs_cfb(op_in, op_load, op_tail, cload, fg=2):
-    cmin = 5e-16
-    cmax = 5e-14
-    fmin = 6
-    fmax = 11
-    num_c = 5
-    num_f = 1000
-
-    scale_load = op_in['ibias'] / op_load['ibias'] * fg
-
-    cir = LTICircuit()
-    cir.add_transistor(op_in, 'mid', 'in', 'gnd', 'gnd', fg=fg)
-    cir.add_transistor(op_load, 'mid', 'gnd', 'gnd', 'gnd', fg=scale_load)
-    cir.add_transistor(op_load, 'out', 'mid', 'gnd', 'gnd', fg=scale_load)
-    cir.add_transistor(op_tail, 'out', 'gnd', 'gnd', 'gnd', fg=fg)
-    cir.add_cap(cload, 'out', 'gnd')
-
-    cfb = np.logspace(np.log10(cmin), np.log10(cmax), num_c).tolist()
-    gfb = op_load['gm'] * scale_load
-    cir.add_conductance(gfb, 'mid', 'x')
-
-    print('fg_in = %d, fg_load=%.3g, rfb = %.4g' % (fg, scale_load, 1/gfb))
-
-    tf_list, lbl_list = [], []
-    for cval in cfb:
-        cir.add_cap(cval, 'x', 'out')
-        tf_list.append(cir.get_num_den('in', 'out'))
-        cir.add_cap(-cval, 'x', 'out')
-        lbl_list.append('$C_{f} = %.3g$f' % (cval * 1e15))
-
-    fvec = np.logspace(fmin, fmax, num_f)
-    plot_tf(fvec, tf_list, lbl_list)
 
 
 def plot_tf(fvec, tf_list, lbl_list):
@@ -79,6 +46,127 @@ def plot_tf(fvec, tf_list, lbl_list):
     plt.show()
 
 
+def opt_cfb(phase_margin, cir, cmin, cmax, cstep, ctol):
+    bin_iter = FloatBinaryIterator(cmin, None, ctol, search_step=cstep)
+    while bin_iter.has_next():
+        cur_cf = bin_iter.get_next()
+        cir.add_cap(cur_cf, 'x', 'out')
+        num, den = cir.get_num_den('in', 'out')
+        cur_pm, _ = get_stability_margins(num, den)
+        if cur_pm < phase_margin:
+            if cur_cf > cmax:
+                # no way to make amplifier stable, just return
+                return None
+            bin_iter.up()
+        else:
+            bin_iter.save()
+            bin_iter.down()
+        cir.add_cap(-cur_cf, 'x', 'out')
+
+    return bin_iter.get_last_save()
+
+
+def tf_vs_cfb(op_in, op_load, op_tail, cload, fg=2):
+    fvec = np.logspace(6, 11, 1000)
+    cvec = np.logspace(np.log10(5e-16), np.log10(5e-14), 5).tolist()
+
+    scale_load = op_in['ibias'] / op_load['ibias'] * fg
+
+    cir = LTICircuit()
+    cir.add_transistor(op_in, 'mid', 'in', 'gnd', 'gnd', fg=fg)
+    cir.add_transistor(op_load, 'mid', 'gnd', 'gnd', 'gnd', fg=scale_load)
+    cir.add_transistor(op_load, 'out', 'mid', 'gnd', 'gnd', fg=scale_load)
+    cir.add_transistor(op_tail, 'out', 'gnd', 'gnd', 'gnd', fg=fg)
+    cir.add_cap(cload, 'out', 'gnd')
+
+    gfb = op_load['gm'] * scale_load
+    cir.add_conductance(gfb, 'mid', 'x')
+
+    print('fg_in = %d, fg_load=%.3g, rfb = %.4g' % (fg, scale_load, 1/gfb))
+
+    tf_list, lbl_list = [], []
+    for cval in cvec:
+        cir.add_cap(cval, 'x', 'out')
+        tf_list.append(cir.get_num_den('in', 'out'))
+        cir.add_cap(-cval, 'x', 'out')
+        lbl_list.append('$C_{f} = %.3g$f' % (cval * 1e15))
+
+    plot_tf(fvec, tf_list, lbl_list)
+
+
+def funity_vs_scale2(op_in, op_load, op_tail, cload, phase_margin, fg=2):
+    s2min = 1
+    s2max = 40
+    num_s = 100
+    cmin = 1e-16
+    cmax = 1e-9
+    ctol = 1e-17
+    cstep = 1e-15
+
+    scale_load = op_in['ibias'] / op_load['ibias'] * fg
+    gfb = op_load['gm'] * scale_load
+    s2vec = np.linspace(s2min, s2max, num_s).tolist()
+    funity_list, pm_list, copt_list = [], [], []
+    for s2 in s2vec:
+        cir = LTICircuit()
+        cir.add_transistor(op_in, 'mid', 'in', 'gnd', 'gnd', fg=fg)
+        cir.add_transistor(op_load, 'mid', 'gnd', 'gnd', 'gnd', fg=scale_load)
+        cir.add_transistor(op_load, 'out', 'mid', 'gnd', 'gnd', fg=scale_load * s2)
+        cir.add_transistor(op_tail, 'out', 'gnd', 'gnd', 'gnd', fg=fg * s2)
+        cir.add_cap(cload, 'out', 'gnd')
+        cir.add_conductance(gfb * s2, 'mid', 'x')
+
+        copt = opt_cfb(phase_margin, cir, cmin, cmax, cstep, ctol)
+        if copt is None:
+            raise ValueError('oops, Cfb is None')
+
+        cir.add_cap(copt, 'x', 'out')
+        num, den = cir.get_num_den('in', 'out')
+
+        funity_list.append(get_w_crossings(num, den)[0] / (2 * np.pi))
+        pm_list.append(get_stability_margins(num, den)[0])
+        copt_list.append(copt)
+
+    f, (ax0, ax1, ax2) = plt.subplots(3, sharex='all')
+    ax0.plot(s2vec, np.array(copt_list) * 1e15)
+    ax0.set_ylabel('Cf (fF)')
+    ax1.plot(s2vec, pm_list)
+    ax1.set_ylabel('$\phi_{PM}$ (deg)')
+    ax2.plot(s2vec, np.array(funity_list) * 1e-9)
+    ax2.set_ylabel('$f_{UG}$ (GHz)')
+    ax2.set_xlabel('$I_2/I_1$')
+    plt.show()
+
+
+def funity_vs_scale2_nocfb(op_in, op_load, op_tail, cload, fg=2):
+    s2min = 1
+    s2max = 40
+    num_s = 100
+
+    scale_load = op_in['ibias'] / op_load['ibias'] * fg
+    s2vec = np.linspace(s2min, s2max, num_s).tolist()
+    funity_list, pm_list = [], []
+    for s2 in s2vec:
+        cir = LTICircuit()
+        cir.add_transistor(op_in, 'mid', 'in', 'gnd', 'gnd', fg=fg)
+        cir.add_transistor(op_load, 'mid', 'gnd', 'gnd', 'gnd', fg=scale_load)
+        cir.add_transistor(op_load, 'out', 'mid', 'gnd', 'gnd', fg=scale_load * s2)
+        cir.add_transistor(op_tail, 'out', 'gnd', 'gnd', 'gnd', fg=fg * s2)
+        cir.add_cap(cload, 'out', 'gnd')
+        num, den = cir.get_num_den('in', 'out')
+
+        funity_list.append(get_w_crossings(num, den)[0] / (2 * np.pi))
+        pm_list.append(get_stability_margins(num, den)[0])
+
+    f, (ax0, ax1) = plt.subplots(2, sharex='all')
+    ax0.plot(s2vec, pm_list)
+    ax0.set_ylabel('$\phi_{PM}$ (deg)')
+    ax1.plot(s2vec, np.array(funity_list) * 1e-9)
+    ax1.set_ylabel('$f_{UG}$ (GHz)')
+    ax1.set_xlabel('$I_2/I_1$')
+    plt.show()
+
+
 def run_main():
     nch_dir = 'data/nch_w4'
     pch_dir = 'data/pch_w4'
@@ -89,6 +177,7 @@ def run_main():
     vmid = vdd / 2
 
     cload = 10e-15
+    phase_margin = 45
 
     nch_db, pch_db = get_db(nch_dir, pch_dir, intent=intent)
 
@@ -106,7 +195,9 @@ def run_main():
     # noinspection PyTypeChecker
     op_tail = nch_db.query(vbs=0, vds=vtail, vgs=vbias)
 
-    tf_vs_cfb(op_in, op_load, op_tail, cload)
+    # tf_vs_cfb(op_in, op_load, op_tail, cload)
+    funity_vs_scale2(op_in, op_load, op_tail, cload, phase_margin, fg=2)
+    # funity_vs_scale2_nocfb(op_in, op_load, op_tail, cload, fg=2)
 
 
 if __name__ == '__main__':
